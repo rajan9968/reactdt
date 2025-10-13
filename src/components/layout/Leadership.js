fb464233030e7a00ea32c68ddf612b8c02f6c9b3
rajan9968 <106149141+rajan9968@users.noreply.github.com>
2025-10-13 18:45:58 +0000

    update css

--- src/components/layout/Leadership.js
+++ src/components/layout/Leadership.js
@@ -31,213 +31,99 @@ export default function Leadership() {
                     </div>
 
                 </section>
-                {/* Progress start  */}
-                <section className="powering-progress vision-section py-5">
+                <section className='leader-section py-5'>
                     <div className="container-fluid plr">
-                        <div className="text-center mb-5">
-                            <h2 className="section-title">
-                                Overview
-                            </h2>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <div className="power-img">
-                                    <img
-                                        src="assets/images/join-1.png"
-                                        className="img-fluid w-100 rounded"
-                                        alt=""
-                                        srcSet=""
-                                    />
-                                </div>
-                            </div>
-                            <div className="col-lg-6 d-flex flex-column">
-                                <div className="power-content mb-auto">
-                                    <p>
-                                        Loremipsumdolorsitamet,consectetueradipiscingelit,seddiamnonummynibheuismodtinciduntut
-                                        laoreetdoloremagnaaliquameratvolutpat.Utwisienimadminimveniam,quisnostrudexercitation
-                                        ullamcorpersuscipitlobortisnislutaliquipexeacommodoconsequat.Duisautemveleumiriuredolorin
-                                        hendreritinvulputatevelitessemolestieconsequat,velillumdoloreeufeugiatnullafacilisisatveroeros
-                                        etaccumsanetiustoodiodignissimquiblanditpraesentluptatumzzrildelenitaugueduisdolorete
-                                        feugait nulla facilisi. </p>
-                                    <p>
-                                        Loremipsumdolorsitamet,consectetueradipiscingelit,seddiamnonummynibheuismodtinciduntut
-                                        laoreetdoloremagnaaliquameratvolutpat.Utwisienimadminimveniam,quisnostrudexercitation
-                                        ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                                        Loremipsumdolorsitamet,consectetueradipiscingelit,seddiamnonummynibheuismodtinciduntut
-                                        laoreetdoloremagnaaliquameratvolutpat.Utwisienimadminimveniam,quisnostrudexercitation
-                                        ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                                    </p>
-                                </div>
-                                <div className="d-flex align-items-end justify-content-end">
-
-                                    <div className="group-icon">
-                                        <img src="assets/images/Group-icon.png" alt="" srcSet="" />
+                        <ul
+                            className="nav nav-pills mb-3 position-unset"
+                            id="pills-tab"
+                            role="tablist"
+                        >
+                            <li className="nav-item" role="presentation">
+                                <button
+                                    className="nav-link active"
+                                    id="pills-home-tab"
+                                    data-bs-toggle="pill"
+                                    data-bs-target="#pills-home"
+                                    type="button"
+                                    role="tab"
+                                    aria-controls="pills-home"
+                                    aria-selected="true"
+                                >
+                                    Key Management
+                                </button>
+                            </li>
+                            <li className="nav-item" role="presentation">
+                                <button
+                                    className="nav-link"
+                                    id="pills-profile-tab"
+                                    data-bs-toggle="pill"
+                                    data-bs-target="#pills-profile"
+                                    type="button"
+                                    role="tab"
+                                    aria-controls="pills-profile"
+                                    aria-selected="false"
+                                >
+                                    Board of Directors
+                                </button>
+                            </li>
+                            <li className="nav-item" role="presentation">
+                                <button
+                                    className="nav-link"
+                                    id="pills-contact-tab"
+                                    data-bs-toggle="pill"
+                                    data-bs-target="#pills-contact"
+                                    type="button"
+                                    role="tab"
+                                    aria-controls="pills-contact"
+                                    aria-selected="false"
+                                >
+                                    Head of Department
+                                </button>
+                            </li>
+                        </ul>
+                        <div className="tab-content" id="pills-tabContent">
+                            <div
+                                className="tab-pane fade show active"
+                                id="pills-home"
+                                role="tabpanel"
+                                aria-labelledby="pills-home-tab"
+                            >
+                                <div className="row">
+                                    <div className="col-lg-4 col-md-6 col-sm-12">
+                                        <div className="team-card shadow rounded-top rounded-bottom bg-light">
+                                            <img
+                                                src="assets/images/team-1.png"
+                                                alt="Mr. Kuldeep Singh"
+                                                className="img-fluid rounded-top  mb-3"
+                                            />
+                                            <div className='team-name'>
+                                                <h5 className="fw-bold mb-1">Mr. Kuldeep Singh</h5>
+                                                <p>Chairman</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div
+                                className="tab-pane fade"
+                                id="pills-profile"
+                                role="tabpanel"
+                                aria-labelledby="pills-profile-tab"
+                            >
+                                ...
+                            </div>
+                            <div
+                                className="tab-pane fade"
+                                id="pills-contact"
+                                role="tabpanel"
+                                aria-labelledby="pills-contact-tab"
+                            >
+                                ...
+                            </div>
                         </div>
                     </div>
                 </section>
-                {/* Progress end  */}
-                <section className="our-business vision-section my-5">
-                    <div className="container-fluid plr">
-                        <div className="row g-5">
-                            {/* Turnkey EPC Projects */}
-                            <div className="col-md-6 col-lg-4 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/Vision-icon.png"
-                                        className="card-image"
-                                        alt="Turnkey EPC Projects"
-                                    />
-                                    <h3>Vision</h3>
-                                    <p>
-                                        powers India’s sustainable goals and shapes its clean
-                                        Driving climate action by building infrastructure that
-                                        energy future.
-                                    </p>
-                                </div>
-                            </div>
-                            {/* Grid Infrastructure */}
-                            <div className="col-md-6 col-lg-4 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/mission-icon.png"
-                                        className="card-image"
-                                        alt="Grid Infrastructure"
-                                    />
-                                    <h3>Mission</h3>
-                                    <p>
-                                        Driving the transition to sustainable energy by developing
-                                        ground-breaking technologies, addressing challenges
-                                        head-on, and l eading the way to a cleaner, greener
-                                        future.
-                                    </p>
-                                </div>
-                            </div>
-                            {/* Independent Power Producer */}
-                            <div className="col-md-6 col-lg-4 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/purpose-icon.png"
-                                        className="card-image"
-                                        alt="Independent Power Producer"
-                                    />
-                                    <h3>Purpose</h3>
-                                    <p>
-                                        purpose-built to contribute to these targets, supporting
-                                        At Datta Infra, our projects and partnerships are
-                                        India’s ‘2030 sustainability vision’ and the broader
-                                        ambition of ‘Viksit Bharat 2047’.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
 
-                    </div>
-                </section>
-                <section className="our-business core-values my-5">
-                    <div className="container-fluid plr">
-                        <div className="text-center mb-5">
-                            <h2 className="section-title">Core Values</h2>
-                        </div>
-                        <div className="row g-5">
-                            {/* Pre-Development EPC */}
-                            <div className="col-md-6 col-lg-3 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/sustainability-icon.png"
-                                        className="card-image"
-                                        alt="Pre-Development EPC"
-                                    />
-                                    <h3>Sustainability</h3>
-                                    <p>
-                                        Commitment to building a cleaner
-                                        future through sustainable energy
-                                        to India’s green transition and goals.
-                                        solutions, contributing meaningfully
-                                    </p>
-                                </div>
-                            </div>
-                            {/* Turnkey EPC Projects */}
-                            <div className="col-md-6 col-lg-3 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/innovation-icon.png"
-                                        className="card-image"
-                                        alt="Turnkey EPC Projects"
-                                    />
-                                    <h3>Innovation</h3>
-                                    <p>
-                                        Redefining possibilities through
-                                        forward-thinking solutions in
-                                        infrastructure and renewable energy.
-                                    </p>
-                                </div>
-                            </div>
-                            {/* Grid Infrastructure */}
-                            <div className="col-md-6 col-lg-3 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/collaboration-icon.png"
-                                        className="card-image"
-                                        alt="Grid Infrastructure"
-                                    />
-                                    <h3>Collaboration</h3>
-                                    <p>
-                                        Achieving shared goals to drive
-                                        impactful change.
-                                    </p>
-                                </div>
-                            </div>
-                            {/* Independent Power Producer */}
-                            <div className="col-md-6 col-lg-3 col-6">
-                                <div className="card-custom">
-                                    <img
-                                        src="assets/images/tntegrity-icon.png"
-                                        className="card-image"
-                                        alt="Independent Power Producer"
-                                    />
-                                    <h3>Integrity</h3>
-                                    <p>
-                                        Ensuring transparency, ethics, and
-                                        compliance with the highest
-                                        standards of Environment, Health,
-                                        and Safety (EHS).
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className="timeline-banner">
-                    <div className="container-fluid plr">
-                        <div className="text-center mb-5">
-                            <h2 className="section-title">Timeline</h2>
-                        </div>
-                        <img
-                            src="assets/images/timeline-banner.png"
-                            alt="awards"
-                            className="img-fluid"
-                            srcSet=""
-                        />
-                    </div>
-                </section>
-                <section className="timeline-banner py-5">
-                    <div className="container-fluid plr">
-                        <div className="text-center mb-5">
-                            <h2 className="section-title">Our Presence</h2>
-                        </div>
-                        <div className='text-center'>
-                            <img
-                                src="assets/images/our-presence-map.png"
-                                alt="awards"
-                                className="img-fluid"
-                                srcSet=""
-                            />
-                        </div>
-                    </div>
-                </section>
             </main>
             <Footer />
         </div>
